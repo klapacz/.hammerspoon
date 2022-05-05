80260dc5ecd59c15c790926c0fcc40c2ab08643e
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-05-05 12:17:14 +0200

    add TODO

--- init.lua
+++ init.lua
@@ -6,3 +6,15 @@ hs.eventtap.new({ hs.eventtap.event.types.leftMouseDown }, function(e)
         hs.eventtap.middleClick(point, 0)
     end 
 end):start()
+
+-- TODO: switch between spaces with cmd + [0-9]
+-- for space = 1, 6 do
+--     hs.hotkey.bind({"cmd"}, tostring(space), function()
+--         local screen = primaryScreen
+--         print(hs.inspect(screen))
+--         local spaces = hs.spaces.spacesForScreen()
+--         -- TODO: check spaces length
+--         local spaceID = spaces[space]
+--         hs.spaces.gotoSpace(spaceID)
+--     end)
+-- end
